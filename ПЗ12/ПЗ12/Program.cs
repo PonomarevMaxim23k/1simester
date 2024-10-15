12b8bd88e16d149e1c18cd2fc63af76c4bee8900
PonomarevMaxim23k <maxosmaxosovich@gmail.com>
2024-10-15 20:52:27 +0300

    ПЗ12
    в решении лежит исправленный файл

---
+++ ПЗ12/ПЗ12/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПЗ12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Численность группы = ");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            int c;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Возраст {0}-го человеака: ", i + 1);
+                c = Convert.ToInt32(Console.ReadLine());
+                if (c < 0 || c >= 200)
+                {
+                    Console.WriteLine("вы ввели несуществующий возраст. Попробуйте снова");
+                    i--;
+                }
+                else
+                {
+                    a[i] = c;
+                }
+            }
+            double m = 0;
+            for (int i = 0; i < n; i++)
+                m = m + a[i];
+            m = m / n;
+            int min, max;
+            min = a[0];
+            max = a[0];
+            for (int i = 1; i < n; i++)
+            {
+                if (a[i] < min)
+                    min = a[i];
+                if (a[i] > max)
+                    max = a[i];
+            }
+            Console.Write("ИТОГИ:\nВсего опрошено {0}\n", n);
+            Console.WriteLine("средний возраст = {0}", m);
+            Console.WriteLine("Минимальный возраст = {0}", min);
+            Console.WriteLine("Максимальный возраст = {0}", max);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ПЗ12/ПЗ12/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ПЗ12")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ПЗ12")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("6ec3fdcf-d8c4-48c4-8457-de9ea945418d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
