389d757a8c0148eaf35b66f1c33e82f1cf3b5e25
PonomarevMaxim23k <maxosmaxosovich@gmail.com>
2024-11-18 08:50:27 +0300

    Add files via upload

---
+++ ПЗ4/Болдырева Пономарев/Болдырева Пономарев/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TypesConvertion
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            string stringValue = Console.ReadLine();
+            int intValue = Convert.ToInt32(stringValue);
+            long longValue = intValue;
+            float floatValue = intValue;
+            Console.WriteLine($"Переменная типа long = {longValue}");
+            Console.WriteLine($"Переменная типа float = {floatValue}");
+        }
+    }
+}
