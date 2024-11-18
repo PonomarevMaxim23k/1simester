29a63fdbc713ceec373bbec4fd419d0778db3dbb
PonomarevMaxim23k <maxosmaxosovich@gmail.com>
2024-11-18 08:43:34 +0300

    ПЗ-3

---
+++ ПЗ-3/program 1/Program.cs
@@ -0,0 +1,14 @@
+﻿// 01_01.cs - Первая программа
+using static System.Console;
+class HelloUser
+{
+    static void Main()
+    {
+        string name;
+        WriteLine("Введите Ваше имя:");
+        name = ReadLine();
+        WriteLine("Приветствую Вас, " + name + "!");
+        WriteLine("для завершения сеанса нажмите ENTER.");
+        ReadLine();
+    }
+}
