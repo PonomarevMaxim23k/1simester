a45cd33b975d3d9df586cbb9d76ab750b9f1c8fb
PonomarevMaxim23k <maxosmaxosovich@gmail.com>
2024-12-02 15:47:22 +0300

    Практическое задание 21

---
+++ ПЗ21/1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    
+    internal class AverageMethods
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 - использовать массив целых чисел, 2 - использовать массив десятичных чисел");
+            Console.WriteLine("Выберите операцию:");
+            string a = Console.ReadLine();
+            
+            
+            if (a == "1")
+            {
+                int[] massive = { 1, 2, 3, 4, 5, 6, 7, 8, 12312,3124,12,1 };
+                Console.WriteLine("Используемый массив: " + massive);
+                Console.WriteLine(Average(massive));
+                Console.ReadLine();
+            }
+            else
+            {
+                float[] massive = { 1F, 2F, 3F,124F };
+                Console.WriteLine("Используемый массив: " + massive);
+                Console.WriteLine(Average(massive));
+                Console.ReadLine();
+            }
+            
+        }
+        
+        static double Average(int[] ints)
+            {
+            double summ = 0.0;
+            for (int i = 0; i < ints.Length; i++)
+            {
+                summ += ints[i];
+            }
+                return summ / ints.Length;
+        }
+        static float Average(float[] floats)
+            {
+                float summ = 0;
+                for (int i = 0; i < floats.Length; i++)
+                {
+                    summ += floats[i];
+                }
+                return summ / floats.Length;
+            }
+
+        
+    }
+}
---
+++ ПЗ21/2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Circle
+    { 
+        int x, y;
+        int radius;
+        static void Main(string[] args)
+        {
+            
+        }
+        public void konstr()
+        {
+            x = y = radius = 0;
+        }
+        public void konstr(int Radius)
+        {
+            x = y = 0;
+            radius = Radius;
+        }
+        public void konstr(int xx, int yy)
+        {
+            x = xx; y = yy;
+            radius = 0;
+        }
+        public void konstr(int xx, int yy, int Radius)
+        {
+            x = xx; y = yy; radius = Radius;
+        }
+    }
+}
