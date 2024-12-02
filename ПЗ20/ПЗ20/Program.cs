c25fdd5ec353430a7ffaf6d875f942b7d4e8acae
PonomarevMaxim23k <maxosmaxosovich@gmail.com>
2024-12-02 15:46:53 +0300

    Практическое задание 20

---
+++ ПЗ20/ПЗ20/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПЗ_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(sumstr());
+            Console.ReadLine();
+        }
+        static int sumstr()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a != 0)
+            {
+                return a+sumstr(); 
+            }
+            else{ return 0; }
+        }
+    }
+}
